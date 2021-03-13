426394062630466578822cde82e881903ce87fc6
sudo doas <doassudo@gmail.com>
2021-03-13 19:30:21 +0200

    added support for playing youtube videos with mpv

--- main.sh
+++ main.sh
@@ -2,6 +2,8 @@
 
 #query=$(cat $HOME/.scripts/googlesearch/dictionary.txt | fzf)
 
+youtubepattern='*youtube*'
+
 echo "type in your search query:"
 read query 
 choice=$(echo $query | sed 's/ /+/g')
@@ -18,9 +20,15 @@ url=$(curl "https://www.google.com/search?sxsrf=ALeKk01YewRFAxy_HI-_kfFyQQ0n6JqH
     fzf) 
 
 
-cd $HOME/.local/tmp/google
-curl $url | pup -c "h1,h2,p" > site.html
-pandoc -f html -t markdown site.html > site.md
-mdcat site.md -p
-
+if [[ "$url" == *youtube* ]]
+then
+    echo "youtube video"
+    mpv $url
+else
+    echo "not a youtube video"
+    cd $HOME/.local/tmp/google
+    curl $url -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Safari/537.36" | pup -c "h1,h2,p,li,ol,ul,pre" > site.html
+    pandoc -f html -t markdown site.html > site.md
+    mdcat site.md -p
+fi
 #brave $url
